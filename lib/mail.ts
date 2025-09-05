a9a09345daa8e3ce712a487eda83cf7d9bc2bda0
Ranith04 <136628558+Ranith04@users.noreply.github.com>
2025-09-05 21:31:49 +0530

    error

--- lib/mail.ts
+++ lib/mail.ts
@@ -2,7 +2,7 @@ import nodemailer from 'nodemailer'
 import type { ContactFormData } from './validators'
 
 // Create transporter
-const transporter = nodemailer.createTransporter({
+const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: parseInt(process.env.SMTP_PORT || '587'),
   secure: false, // true for 465, false for other ports
